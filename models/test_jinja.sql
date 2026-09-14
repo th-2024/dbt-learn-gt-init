c0d67b64988f9dbd619cd8b601583e756765f63a
th-2024 <therluc@jems-group.com>
2026-09-14 10:26:46 +0200

    last modifs

---
+++ models/test_jinja.sql
@@ -0,0 +1,11 @@
+{% set temperature = 80.0%}
+
+On a day like htis, I especially like 
+
+{% if temperature > 70 %}
+a refreshing lemon sorbet.
+
+{% else %}
+a decadent chocolate cake.
+
+{% endif %}
